c684dbba852f09b8a8159b9676391da3545b23e7
Hatta Yuthafiga <crbru.yuthafiga@gmail.com>
2017-12-07 15:17:12 +0900

    Update Version

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -110,8 +110,8 @@ const size_t   BLOCKS_IDS_SYNCHRONIZING_DEFAULT_COUNT        =  10000;  //by def
 const size_t   BLOCKS_SYNCHRONIZING_DEFAULT_COUNT            =  200;    //by default, blocks count in blocks downloading
 const size_t   COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT         =  1000;
 
-const int      P2P_DEFAULT_PORT                              = 23888;
-const int      RPC_DEFAULT_PORT                              = 24888;
+const int      P2P_DEFAULT_PORT                              = 8057;
+const int      RPC_DEFAULT_PORT                              = 8058;
 
 const size_t   P2P_LOCAL_WHITE_PEERLIST_LIMIT                =  1000;
 const size_t   P2P_LOCAL_GRAY_PEERLIST_LIMIT                 =  5000;
